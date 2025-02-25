a7fde8d6a9ed8397ede9064be05e4e94a263c726
lealembts <lealemb84@gmail.com>
2025-02-26 02:12:52 +0300

    header part

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,14 @@ function Header() {
                 <a href="/">
                     <img src="https://pngimg.com/uploads/amazon/small/amazon_PNG11.png" alt="amazon logo" />
                 </a>
+               { /* Delivery */}
+                    <span>
+                        {/* icon */ }   
+                    </span>
+                    <div>
+                        <p>Deliverd to</p>
+                        <span>Ethiopia</span>
+                    </div>
 
             </div>
             <div></div>
