7619ac669b258464e8a9aa6fd8e42e8a252864ef
lealembts <lealemb84@gmail.com>
2025-03-01 20:50:04 +0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header"
-import Test1 from "./components/test/Test1"
-import Test2 from "./components/test/Test2"
+// import Test1 from "./components/test/Test1"
+// import Test2 from "./components/test/Test2"
 
 
 function App() {
